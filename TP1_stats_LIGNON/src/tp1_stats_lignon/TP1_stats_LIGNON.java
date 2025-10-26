a1eb4712d3b31a6ada16f1d7deaf3330c41da54e
Ronan LIGNON <ronan.lignon@epfedu.fr>
2025-10-26 14:07:17 +0100

    TP1_Statistiques  (26/10/2025)

--- TP1_stats_LIGNON/src/tp1_stats_lignon/TP1_stats_LIGNON.java
+++ TP1_stats_LIGNON/src/tp1_stats_lignon/TP1_stats_LIGNON.java
@@ -2,6 +2,9 @@
 
 package tp1_stats_lignon;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author ronan
@@ -12,7 +15,23 @@ public class TP1_stats_LIGNON {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Random generateurAleat = new Random();
+        double tab [] = new double[6];
+        int nb;
+        int m;
+        System.out.println("choicir un nombre");
+        m=sc.nextInt();
+        for (int i =0 ; i<m ; i++){
+            nb = generateurAleat.nextInt(5);
+            tab [nb]+=1;
+        }
+        for (int j =0 ; j<6 ; j++){
+            tab[j]=(tab[j]/m)*100;
+        }
+        for (int i =0 ; i<6 ; i++){
+             System.out.println(tab[i]+" %");
+        }
     }
     
 }
